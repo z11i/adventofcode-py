54c3fbaad9175880964967c523cdc4666a048698
z11i <z11imdk@gmail.com>
2023-11-22 22:56:20 +0800

    2022 Day 2

---
+++ 2022/day02.py
@@ -0,0 +1,36 @@
+from library.input import read
+
+
+def score(opponent_move, player_move):
+    #### R, P, S
+    scores = [
+        [4, 8, 3],  # rock: 1
+        [1, 5, 9],  # paper: 2
+        [7, 2, 6],  # scissors: 3
+    ]
+    return scores[ord(opponent_move) - ord("A")][ord(player_move) - ord("X")]
+
+
+def solve(lines, score_func):
+    return sum(
+        score_func(opponent_move, player_move)
+        for line in lines
+        if line != ""
+        for opponent_move, player_move in [line.split(" ")]
+    )
+
+
+def score_2(opponent_shape, want_result):
+    #### L0,D3,W6
+    scores = [
+        [3, 4, 8],  # rock: 1
+        [1, 5, 9],  # paper: 2
+        [2, 6, 7],  # scissors: 3
+    ]
+    return scores[ord(opponent_shape) - ord("A")][ord(want_result) - ord("X")]
+
+
+if __name__ == "__main__":
+    data = read(ignore_last=True)
+    print(solve(data, score))
+    print(solve(data, score_2))
--- library/input.py
+++ library/input.py
@@ -1,8 +1,11 @@
 from typing import List, Optional
 
 
-def read(filename: Optional[str] = None) -> List[str]:
+def read(filename: Optional[str] = None, ignore_last: bool = False) -> List[str]:
     if filename is None:
         filename = "input.txt"
     with open(filename) as f:
-        return f.read().split("\n")
+        lines = f.read().split("\n")
+        if ignore_last:
+            lines = lines[:-1]
+        return lines
