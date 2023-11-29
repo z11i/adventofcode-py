0514521d1315cdb7293ad870322859422682a4e5
z11i <z11imdk@gmail.com>
2023-11-29 21:56:27 +0800

    Read both input files

--- library/input.py
+++ library/input.py
@@ -1,4 +1,12 @@
-from typing import List, Optional
+from dataclasses import dataclass
+import sys
+from typing import List, Optional, TextIO, TypedDict
+
+
+@dataclass
+class InputResult:
+    example: List[str]
+    puzzle: List[str]
 
 
 def read(filename: Optional[str] = None) -> List[str]:
@@ -7,3 +15,24 @@ def read(filename: Optional[str] = None) -> List[str]:
     with open(filename) as f:
         lines = f.read().split("\n")[:-1]
         return lines
+
+
+def read2(year: Optional[int] = None, day: Optional[int] = None) -> InputResult:
+    # when called like `python3 -m 2022.day04`, sys.argv looks like:
+    # ['/Users/z11i/adventofcode-py/2022/day04.py']
+    year_str, day_str = sys.argv[0].split("/")[-2:]
+    if year and day:
+        year_str, day_str = f"{year:02}", f"{day:02}"
+    day_input_prefix = "/input/".join([year_str, day_str]).split(".")[0]
+    input_path = f"{day_input_prefix}.input"
+    sample_path = f"{day_input_prefix}.example"
+
+    def r(f: TextIO):
+        lines = f.read().split("\n")
+        for i in range(len(lines) - 1, -1, -1):
+            if lines[i] != "":
+                return lines[: i + 1]
+        return lines
+
+    with open(input_path) as fi, open(sample_path) as fs:
+        return InputResult(example=r(fs), puzzle=r(fi))
--- library/prelude.py
+++ library/prelude.py
@@ -1,4 +1,4 @@
-from .input import read
+from .input import read, read2
 from pprint import pprint as print
 
-__all__ = ["read", "print"]
+__all__ = ["read", "read2", "print"]
