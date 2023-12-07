63f905391b1d5c0fa052d5afb389b5d1ad299a7f
z11i <z11imdk@gmail.com>
2023-12-07 23:55:42 +0800

    2023 Day 2

---
+++ 2023/day02.py
@@ -0,0 +1,53 @@
+from math import prod
+from typing import List
+
+import library.prelude as _
+
+idx_map = {
+    "red": 0,
+    "green": 1,
+    "blue": 2,
+}
+
+
+def parse(line: str):
+    game_raw, records = line.split(": ")
+    _, game_id = game_raw.split(" ")
+    record_split = records.split("; ")
+    all_counts = []
+    for record in record_split:
+        count_and_colors = record.split(", ")
+        counts = [0, 0, 0]
+        for count_and_color in count_and_colors:
+            count, color = count_and_color.split(" ")
+            counts[idx_map[color]] = int(count)
+        all_counts.append(counts)
+    return game_id, all_counts
+
+
+def solve(input_data: List[str]):
+    return sum(
+        map(
+            lambda x: int(x[0]),
+            filter(
+                lambda y: all(x[0] <= 12 and x[1] <= 13 and x[2] <= 14 for x in y[1]),
+                map(parse, input_data),
+            ),
+        )
+    )
+
+
+def solve2(input_data: List[str]):
+    return sum(
+        [
+            prod([max(values) for values in zip(*lists)])
+            for _, lists in map(parse, input_data)
+        ]
+    )
+
+
+inputs = _.read2()
+_.print(solve(inputs.example))
+_.print(solve(inputs.puzzle))
+_.print(solve2(inputs.example))
+_.print(solve2(inputs.puzzle))
