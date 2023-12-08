b5d39eb3893c7c2550f8537f31d43fa109cbec99
z11i <z11imdk@gmail.com>
2023-12-08 20:27:15 +0800

    template

---
+++ day00.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import library.prelude as _
+
+
+def solve(input_data: List[str]):
+    pass
+
+
+def solve2(input_data: List[str]):
+    pass
+
+
+inputs = _.read2()
+_.print(solve(inputs.example))
+_.print(solve(inputs.puzzle))
+_.print(solve2(inputs.example))
+_.print(solve2(inputs.puzzle))
